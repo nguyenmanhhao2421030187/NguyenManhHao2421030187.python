a22b207af7ecd11321ac9e131e243db40ef187ec
nguyenmanhhao2421030187 <haodaingusidan@gmail.com>
2026-05-22 20:17:25 +0700

    Add files via upload

---
+++ Chuong 1/vd01.py
@@ -0,0 +1,7 @@
+# ví dụ 1
+str1 = "ví dụ hàm find trong python"
+str2 = "find"
+
+print (str1.find(str2))
+print (str1.find(str2,10))
+print (str1.find(str2,20))
---
+++ Chuong 1/vd05.py
@@ -0,0 +1,6 @@
+# vi du 5
+my_str = "Hello world"
+print(my_str.split(' '))
+print("A B C D E ".split(" "))
+print("A B C D E ".split(" ", 1))
+print("A B C D E ".split(" ", 3))
